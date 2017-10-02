cd6b007b1ecd11e5d7158f29c74814868a7c3d48
brunocardosor <brun.oana@hotmail.com>
2017-10-02 00:05:48 -0300

    Envio de denúncia 70% programada.

---
+++ app/src/main/java/com/example/administrador/citycaremobile/Fragments/EditProfileCidadaoFragment.java
@@ -0,0 +1,27 @@
+package com.example.administrador.citycaremobile.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrador.citycaremobile.R;
+
+
+public class EditProfileCidadaoFragment extends Fragment {
+
+    public EditProfileCidadaoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_edit_profile_cidadao, container, false);
+
+
+        return view;
+    }
+
+}
