32c06a971bf33e32dd41aaf953aa35a978c2fa57
brunocardosor <brun.oana@hotmail.com>
2017-10-10 23:24:48 -0300

    Método de cadastrado atualizado

--- app/src/main/java/com/example/administrador/citycaremobile/Services/Service.java
+++ app/src/main/java/com/example/administrador/citycaremobile/Services/Service.java
@@ -36,7 +36,7 @@ public interface Service {
     @Multipart
     Call<Void> postCidadao(@Header("X-Token") String token,
                            @Part MultipartBody.Part foto,
-                           @Part MultipartBody.Part cidadao);
+                           @Part("cidadao") MultipartBody.Part cidadao);
 
     @PUT("cidadao/put")
     Call<Boolean> putUsuario(@Header("Content-type") String content,
