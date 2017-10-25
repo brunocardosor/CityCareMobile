e80bd5d1fe85e01866080b5f995f27a31c851ad9
brunocardosor <brun.oana@hotmail.com>
2017-10-25 02:28:01 -0200

    Erros de IntentChooser fixados, ajuste da imageView de acordo com a foto funcionando.

--- app/src/main/java/com/example/administrador/citycaremobile/Activities/MainActivity.java
+++ app/src/main/java/com/example/administrador/citycaremobile/Activities/MainActivity.java
@@ -48,68 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
         btEntrar = (TextView) findViewById(R.id.move_to_login);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
 
         View headerView = navDrawer.getHeaderView(0);
         Menu menuView = navDrawer.getMenu();
 
         nomeNavView = (TextView) headerView.findViewById(R.id.name_nav);
         picProfileNav = (CircleImageView) headerView.findViewById(R.id.pic_profile_drawer);
-
-        if (UsuarioApplication.getInstance().getUsuario() != null) {
-            btEntrar.setVisibility(View.GONE);
-            btEntrar.setClickable(false);
-            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-            toolbar.setTitle("");
-            toolbar.setNavigationIcon(R.drawable.ic_drawable_menu);
-            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    drawerLayout.openDrawer(Gravity.START);
-                }
-            });
-            if(UsuarioApplication.getInstance().getUsuario() instanceof Empresa){
-                Empresa empresa = (Empresa) UsuarioApplication.getInstance().getUsuario();
-                nomeNavView.setText(empresa.getNomeFantasia());
-                Glide.with(this).load(empresa.getDirFotoUsuario()).into(picProfileNav);
-            } else {
-                Cidadao cidadao = (Cidadao) UsuarioApplication.getInstance().getUsuario();
-                nomeNavView.setText(cidadao.getNome() + cidadao.getSobrenome());
-                Glide.with(this).load(cidadao.getDirFotoUsuario()).into(picProfileNav);
-            }
-        }
-
-        menuView.getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if(item.getItemId() == R.id.edit_profile){
-                    Intent i = new Intent(MainActivity.this,EditProfileActivity.class);
-                    startActivity(i);
-                }
-                return true;
-            }
-        });
-
-        menuView.getItem(1).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if(item.getItemId() == R.id.bt_logout){
-                    UsuarioApplication.getInstance().logout();
-                    onRestart();
-                }
-                return true;
-            }
-        });
-
-
-        btEntrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, AcessoActivity.class);
-                startActivity(i);
-            }
-        });
-
 
         //TAB LAYOUT
         TabLayout tabLayout = (TabLayout) findViewById(R.id.mTabLayout);
@@ -142,6 +86,64 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        if (UsuarioApplication.getInstance().getUsuario() != null) {
+            btEntrar.setVisibility(View.GONE);
+            btEntrar.setClickable(false);
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            toolbar.setTitle("");
+            toolbar.setNavigationIcon(R.drawable.ic_drawable_menu);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawerLayout.openDrawer(Gravity.START);
+                }
+            });
+            if(UsuarioApplication.getInstance().getUsuario() instanceof Empresa){
+                Empresa empresa = (Empresa) UsuarioApplication.getInstance().getUsuario();
+                nomeNavView.setText(empresa.getNomeFantasia());
+                Glide.with(this).load(empresa.getDirFotoUsuario()).into(picProfileNav);
+            } else {
+                Cidadao cidadao = (Cidadao) UsuarioApplication.getInstance().getUsuario();
+                nomeNavView.setText(cidadao.getNome() + cidadao.getSobrenome());
+                Glide.with(this).load(cidadao.getDirFotoUsuario()).into(picProfileNav);
+            }
+        } else {
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        }
+
+        menuView.getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.edit_profile){
+                    Intent i = new Intent(MainActivity.this,EditProfileActivity.class);
+                    startActivity(i);
+                }
+                return true;
+            }
+        });
+
+        menuView.getItem(1).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.bt_logout){
+                    UsuarioApplication.getInstance().logout();
+                    toolbar.setNavigationIcon(null);
+                    btEntrar.setVisibility(View.VISIBLE);
+                    drawerLayout.closeDrawer(Gravity.START);
+                }
+                return true;
+            }
+        });
+
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AcessoActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -166,6 +168,7 @@ public class MainActivity extends AppCompatActivity {
                     drawerLayout.openDrawer(GravityCompat.START);
                 }
             });
+
             if(UsuarioApplication.getInstance().getUsuario() instanceof Empresa){
                 Empresa empresa = (Empresa) UsuarioApplication.getInstance().getUsuario();
                 nomeNavView.setText(empresa.getNomeFantasia());
