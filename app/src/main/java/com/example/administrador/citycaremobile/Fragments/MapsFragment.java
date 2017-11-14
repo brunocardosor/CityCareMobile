0bde66b07f4e5b145223b4556e8a96a496f36cf9
brunocardosor <brun.oana@hotmail.com>
2017-11-14 01:59:33 -0200

    Mapa carregando normalmente.

--- app/src/main/java/com/example/administrador/citycaremobile/Fragments/MapsFragment.java
+++ app/src/main/java/com/example/administrador/citycaremobile/Fragments/MapsFragment.java
@@ -1,19 +1,20 @@
 package com.example.administrador.citycaremobile.Fragments;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.example.administrador.citycaremobile.Exceptions.APIError;
 import com.example.administrador.citycaremobile.Modelo.Cidadao;
 import com.example.administrador.citycaremobile.Modelo.Empresa;
@@ -30,7 +31,6 @@ import com.example.administrador.citycaremobile.R;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
@@ -40,11 +40,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 import es.dmoral.toasty.Toasty;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.http.POST;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
@@ -58,7 +61,8 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private static MapsFragment instance = null;
     private static GoogleMap gMap;
     private HashMap<Integer, Marker> hashMap;
-
+    private Map<Marker, Postagem> postagemMap = new HashMap<>();
+    private InfoWindowMapsAdapter infoWindowMapsAdapter;
 
     public MapsFragment() {
         if(instance == null){
@@ -86,6 +90,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
+        infoWindowMapsAdapter = new InfoWindowMapsAdapter(getActivity().getLayoutInflater(),postagemMap);
         Geocoder geo = new Geocoder(getContext());
         if(UsuarioApplication.getInstance().getUsuario() != null){
             if(UsuarioApplication.getInstance().getUsuario() instanceof Cidadao){
@@ -112,6 +117,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                 e.printStackTrace();
             }
         }
+        gMap.setInfoWindowAdapter(infoWindowMapsAdapter);
         gMap.setMinZoomPreference(13);
         getMapsData();
     }
@@ -129,8 +135,8 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         } else if (postagem.getDenuncia().isStatusDenuncia() == 0) {
             marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker_done));
         }
-        gMap.setInfoWindowAdapter(new InfoWindowMapsAdapter(getActivity().getLayoutInflater(), postagem));
         hashMap.put(postagem.getDenuncia().getIdDenuncia(), marker);
+        infoWindowMapsAdapter.addPostagem(marker,postagem);
     }
 
     public void removeMarker(Postagem postagem) {
@@ -138,24 +144,25 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         marker.remove();
         postagens.remove(postagem);
         hashMap.remove(postagem);
+        infoWindowMapsAdapter.removePostagem(marker);
     }
 
 
-    private class InfoWindowMapsAdapter implements GoogleMap.InfoWindowAdapter {
-
-        private Postagem post;
+    private static class InfoWindowMapsAdapter implements GoogleMap.InfoWindowAdapter {
         private View view;
 
         private TextView tvCategoria;
-        private ImageView imageViewDenuncia;
+        private TextView tvAgilizas;
+        private TextView tvComentarios;
+        private static Map<Marker,Postagem> mapPostagem;
 
-        InfoWindowMapsAdapter(LayoutInflater inflater, Postagem post) {
+        InfoWindowMapsAdapter(LayoutInflater inflater, Map<Marker, Postagem> postagemMap) {
             view = inflater.inflate(R.layout.info_window_maps, null);
-            this.post = post;
-            imageViewDenuncia = (ImageView) view.findViewById(R.id.info_window_image_maps);
+            tvAgilizas = (TextView) view.findViewById(R.id.tv_agilizas);
+            tvComentarios = (TextView) view.findViewById(R.id.tv_comentarios);
             tvCategoria = (TextView) view.findViewById(R.id.info_window_text_maps);
+            mapPostagem = postagemMap;
         }
-
 
         @Override
         public View getInfoWindow(Marker marker) {
@@ -164,44 +171,51 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
         @Override
         public View getInfoContents(Marker marker) {
+            Postagem post = mapPostagem.get(marker);
             tvCategoria.setText(post.getDenuncia().getCategoriaDenuncia().getDescricaoCategoria());
-            Glide.with(view).load(post.getDenuncia().getDirFotoDenuncia()).into(imageViewDenuncia);
+            tvAgilizas.setText(String.valueOf(post.getAgilizas().size()));
+            tvComentarios.setText(String.valueOf(post.getComentarios().size()));
             return view;
         }
 
+        public void addPostagem(Marker marker, Postagem post){
+            mapPostagem.put(marker,post);
+        }
 
+        public void removePostagem(Marker marker){
+            mapPostagem.remove(marker);
+        }
     }
-
-    private void getMapsData() {
-        Service service = CallService.createService(Service.class);
-        final Call<ArrayList<Postagem>> getMapsData = service.mapsPostagens(UsuarioApplication.getToken());
-        getMapsData.enqueue(new Callback<ArrayList<Postagem>>() {
-            @Override
-            public void onResponse(Call<ArrayList<Postagem>> call, Response<ArrayList<Postagem>> response) {
-                if (response.isSuccessful()) {
-                    gMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getContext(), R.raw.map_style));
-                    setData(response.body());
-                    for(Postagem p : postagens){
-                        addMarker(p);
+        private void getMapsData() {
+            Service service = CallService.createService(Service.class);
+            Call<ArrayList<Postagem>> getMapsData = service.mapsPostagens(UsuarioApplication.getToken());
+            getMapsData.enqueue(new Callback<ArrayList<Postagem>>() {
+                @Override
+                public void onResponse(Call<ArrayList<Postagem>> call, Response<ArrayList<Postagem>> response) {
+                    if (response.isSuccessful()) {
+                        gMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getContext(), R.raw.map_style));
+                        setData(response.body());
+                        for (Postagem p : postagens) {
+                            addMarker(p);
+                        }
+                        return;
+                    } else if (response.code() == 401) {
+                        new SystemUtils().authenticateToken(getContext());
+                        getMapsData();
+                    } else {
+                        APIError error = ErrorUtils.parseError(response);
+                        Log.e("getToken", error.getMessage());
+                        Toasty.error(getContext(), "Erro na conexão", Toast.LENGTH_LONG).show();
                     }
-                    return;
-                } else if (response.code() == 401) {
-                    new SystemUtils().authenticateToken(getContext());
-                    getMapsData();
-                } else {
-                    APIError error = ErrorUtils.parseError(response);
-                    Log.e("getToken", error.getMessage());
+                }
+
+                @Override
+                public void onFailure(Call<ArrayList<Postagem>> call, Throwable t) {
+                    Log.e("getToken", t.getMessage());
                     Toasty.error(getContext(), "Erro na conexão", Toast.LENGTH_LONG).show();
                 }
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<Postagem>> call, Throwable t) {
-                Log.e("getToken", t.getMessage());
-                Toasty.error(getContext(), "Erro na conexão", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
+            });
+        }
 
     @Override
     public void onResume() {
